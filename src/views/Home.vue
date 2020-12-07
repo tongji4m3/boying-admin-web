b6688e8441e12779e43dd08788b17ff719bdfe8f
方浩 <7933896+F_HGitee@user.noreply.gitee.com>
2020-12-07 19:12:10 +0800

    home报表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,48 @@
 <template>
-  <div >
-    这里是主页
-    <div class="total-layout">
-      <el-row :gutter="20">
-        <el-col :span="6">
-          <div class="total-frame">
-            <i class="el-icon-s-order"></i>
-            <div class="total-title">今日订单总数</div>
-            <div class="total-value">
-              {{todayOrderNum}}
+  <div v-loading="loading" style="padding: 50px">
+    <div>
+      <div class="total-layout">
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <div class="total-frame">
+              <div class="total-icon">
+                <i class="el-icon-s-order"></i>
+              </div>
+              <div class="total-title">今日订单总数</div>
+              <div class="total-value">
+                {{ todayOrderNum }}
+              </div>
             </div>
-          </div>
-        </el-col>
-        <el-col :span="6">
-          <div class="total-frame">
-            <i class="el-icon-coin"></i>
-            <div class="total-title">今日销售总额</div>
-            <div class="total-value" >{{todayOrderMoney}}</div>
-          </div>
-        </el-col>
-        <el-col :span="6">
-          <div class="total-frame">
-            <i class="el-icon-coin"></i>
-            <div class="total-title">昨日销售总额</div>
-            <div class="total-value">{{YesterOrderMoney}}</div>
-          </div>
-        </el-col>
-      </el-row>
+          </el-col>
+          <el-col :span="6">
+            <div class="total-frame">
+              <div class="total-icon">
+                <i class="el-icon-coin"></i>
+              </div>
+              <div class="total-title">今日销售总额</div>
+              <div class="total-value">{{ todayOrderMoney }}</div>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="total-frame">
+              <div class="total-icon">
+                <i class="el-icon-coin"></i>
+              </div>
+              <div class="total-title">平台销售总额</div>
+              <div class="total-value">{{ allOrderMoney }}</div>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="total-frame">
+              <div class="total-icon">
+                <i class="el-icon-user-solid"></i>
+              </div>
+              <div class="total-title">今日新增用户</div>
+              <div class="total-value">{{ todayUserNum }}</div>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
     </div>
   </div>
 </template>
@@ -38,12 +54,43 @@
 
 .el-icon-s-order {
   font-size: 50px;
-  margin-top: 50px;
 }
 
 .el-icon-coin {
   font-size: 50px;
-  margin-top: 50px;
+}
+
+.total-layout {
+  margin-top: 20px;
+}
+.total-frame {
+  border: 1px solid #dcdfe6;
+  padding: 20px;
+  height: 80px;
+  padding-right: 130px;
+  padding-top: 60px;
+}
+
+.total-title {
+  position: relative;
+  font-size: 16px;
+  color: #909399;
+  left: 70px;
+  top: -50px;
+}
+
+.total-value {
+  position: relative;
+  font-size: 18px;
+  color: #606266;
+  left: 70px;
+  top: -40px;
+}
+
+.total-icon {
+  color: #409eff;
+  width: 60px;
+  height: 60px;
 }
 
 .item {
@@ -64,50 +111,92 @@
 import axios from "axios";
 import api from "@/assets/api.js";
 import qs from "qs";
+import moment from "moment";
 
 export default {
   data() {
     return {
-      todayOrderNum:10,
-      todayOrderMoney:10,
-      YesterOrderMoney:10,
-      date:new Date(),
+      todayOrderNum: 10,
+      todayOrderMoney: 10,
+      allOrderMoney: 10,
+      todayUserNum: 10,
+      date: new Date(),
+      loading: true,
     };
   },
-  mounted:function(){
+  mounted: function () {
     this.loadTodayOrder();
     this.loadTodaySale();
+    this.loadthisMonthSale();
+    this.loadTodayUserStatistics();
   },
   methods: {
-    async loadTodayOrder(){
+    async loadTodayOrder() {
       const res = await axios.post(
-          `${api.API_URL}/statistics/dayOrder`,
-          this.date,{
+        `${api.API_URL}/statistics/dayOrder`,
+        this.date,
+        {
           headers: {
             Authorization: "Bearer " + sessionStorage.getItem("token"),
-          }}
-        );
-        console.log(res);
-        if (res.data.code == 200) {
-          this.todayOrderNum=res.data.data;
+          },
         }
+      );
+      console.log(res);
+      if (res.data.code == 200) {
+        this.todayOrderNum = res.data.data;
+      }
     },
 
-    async loadTodaySale(){
+    async loadTodaySale() {
       const res = await axios.post(
-          `${api.API_URL}/statistics/dayOrderMoney`,
-          this.date,{
+        `${api.API_URL}/statistics/dayOrderMoney`,
+        this.date,
+        {
           headers: {
             Authorization: "Bearer " + sessionStorage.getItem("token"),
-          }}
-        );
-        console.log(res);
-        if (res.data.code == 200) {
-          this.todayOrderMoney=res.data.data;
+          },
         }
-    }
+      );
+      console.log(res);
+      if (res.data.code == 200) {
+        this.todayOrderMoney = res.data.data;
+      }
+    },
 
-    
+    async loadthisMonthSale() {
+      const res = await axios.post(
+        `${api.API_URL}/statistics/AllOrderMoney`,
+        {},
+        {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        }
+      );
+      console.log(res);
+      if (res.data.code == 200) {
+        this.allOrderMoney = res.data.data;
+        setTimeout(() => {
+          this.loading = false;
+        }, 1000);
+      }
+    },
+
+    async loadTodayUserStatistics() {
+      const res = await axios.post(
+        `${api.API_URL}/statistics/userDailyGrowth`,
+        this.date,
+        {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        }
+      );
+      console.log(res);
+      if (res.data.code == 200) {
+        this.todayUserNum = res.data.data;
+      }
+    },
   },
 };
 </script>
