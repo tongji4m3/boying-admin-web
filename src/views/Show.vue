ee792abc2e1a184ddc520030bcd97a09ab677a45
方浩 <7933896+F_HGitee@user.noreply.gitee.com>
2020-12-08 20:28:48 +0800

    show只剩编辑

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,5 +1,176 @@
 <template>
-  <div>
-    这里是演出列表
-  </div>
+  <el-table
+    :key="key"
+    :data="
+      tableData.filter(
+        (data) =>
+          !search || data.name.toLowerCase().includes(search.toLowerCase())
+      )
+    "
+    v-loading="loading"
+    style="width: 100%"
+  >
+    <el-table-column
+      v-for="fruit in formThead"
+      :key="fruit.prop"
+      :label="fruit.label"
+      :width="fruit.width"
+      show-overflow-tooltip
+    >
+      <template slot-scope="scope">
+        {{ scope.row[fruit.prop] }}
+      </template>
+    </el-table-column>
+    <!-- <el-table-column label="演出编号" prop="showId"> </el-table-column>
+    <el-table-column label="演出名称" prop="name"> </el-table-column>
+    <el-table-column label="演出开始时间" prop="dayStart">
+    </el-table-column>
+    <el-table-column label="演出海报" prop="poster"> </el-table-column>
+    <el-table-column label="演出地址" prop="showAddress"> </el-table-column> -->
+    <el-table-column align="right">
+      <template slot="header" slot-scope="scope">
+        <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
+      </template>
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)"
+          >Delete</el-button
+        >
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
+
+<script>
+import axios from "axios";
+import api from "@/assets/api.js";
+import qs from "qs";
+const fields = [
+  { label: "演出编号", prop: "showId" },
+  { label: "演出名称", prop: "name" },
+  { label: "演出目录编号", prop: "categoryId" },
+  { label: "演出海报", prop: "poster" },
+  // { label: "演出详情", prop: "showDetails" },
+  { label: "演出最低价格", prop: "minPrice" },
+  // { label: "演出最低价格", prop: "showMinPrice" },
+  // { label: "演出最高价格", prop: "showMaxPrice" },
+  // { label: "演出优先级", prop: "showWeight" },
+  // { label: "演出城市", prop: "showCity" },
+  { label: "演出地址", prop: "address" },
+  // { label: "演出时间", prop: "showTime" },
+  { label: "演出开始时间", prop: "dayStart" },
+  // { label: "演出结束时间", prop: "showEndTime" },
+];
+
+export default {
+  async mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
+    this.reload();
+  },
+  data() {
+    return {
+      tableData: [],
+      search: "",
+      key: 1, // table key
+      formThead: fields, // 默认表头 Default header
+      loading: true,
+    };
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    async handleDelete(index, row) {
+      console.log(row.showId);
+      try {
+        console.log("mounted");
+        const res = await axios.post(`${api.API_URL}/show/delete/`+row.showId, {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        });
+        console.log("test")
+        console.log(res);
+        if (res.data.code == 200) {
+          this.$message.success("删除成功");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+      this.tableData.splice(index, 1);
+      console.log(index, row);
+    },
+    async reload() {
+      try {
+        console.log("mounted");
+        const res = await axios.get(`${api.API_URL}/show/list`, {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        });
+        console.log(res);
+        if (res.data.code == 200) {
+          this.tableData = res.data.data;
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+    /**
+     * 遍历列的所有内容，获取最宽一列的宽度
+     * @param arr
+     */
+    getMaxLength(arr) {
+      return arr.reduce((acc, item) => {
+        if (item) {
+          let calcLen = this.getTextWidth(item);
+          if (acc < calcLen) {
+            acc = calcLen;
+          }
+        }
+        return acc;
+      }, 0);
+    },
+    /**
+     * 使用span标签包裹内容，然后计算span的宽度 width： px
+     * @param valArr
+     */
+    getTextWidth(str) {
+      let width = 0;
+      let html = document.createElement("span");
+      html.innerText = str;
+      html.className = "getTextWidth";
+      document.querySelector("body").appendChild(html);
+      width = document.querySelector(".getTextWidth").offsetWidth;
+      document.querySelector(".getTextWidth").remove();
+      return width;
+    },
+  },
+  watch: {
+    /**
+     * 监控表格的数据data，自动设置表格宽度
+     * @param valArr
+     */
+    data(valArr) {
+      const _this = this;
+      this.formThead = fields.map(function (value) {
+        const arr = valArr.map((x) => x[value.prop]); // 获取每一列的所有数据
+        arr.push(value.label); // 把每列的表头也加进去算
+        value.width = _this.getMaxLength(arr) + 20; // 每列内容最大的宽度 + 表格的内间距(依据实际情况而定)
+        return value;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.el-button {
+  text-size-adjust: auto;
+}
+</style>
