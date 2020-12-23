3374f1c528761e8bff3569f8506754a96da88b20
fh <1341710779@qq.com>
2020-12-23 17:17:27 +0800

    api改远程

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,5 +1,5 @@
 export default {
-    API_URL: "http://localhost:7000"
-    // API_URL: "http://47.103.203.188:7000"
+    // API_URL: "http://localhost:7000"
+    API_URL: "http://47.103.203.188:7000"
   };
   
