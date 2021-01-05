3862072397935eace0490afdd61c5d398a2c8bce
Cffuu <1289079621@qq.com>
2021-01-06 01:44:13 +0800

    报表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -47,7 +47,7 @@
       <div class="statistics-layout">
         <div class="layout-title">订单统计</div>
         <el-row>
-          <el-col :span="4">
+          <!-- <el-col :span="4">
             <div style="padding: 20px">
               <div>
                 <div style="color: #909399; font-size: 14px">本月订单总数</div>
@@ -94,7 +94,7 @@
                 </div>
               </div>
             </div>
-          </el-col>
+          </el-col> -->
           <el-col :span="20">
             <div style="padding: 10px; border-left: 1px solid #dcdfe6">
               <el-date-picker
@@ -205,27 +205,10 @@ import qs from "qs";
 import moment from "moment";
 import { str2Date } from "@/utils/date";
 
-//画图用
-const DATA_FROM_BACKEND = {
-  columns: ["date", "orderCount", "orderAmount"],
-  rows: [
-    { date: "2018-11-01", orderCount: 10, orderAmount: 1093 },
-    { date: "2018-11-02", orderCount: 20, orderAmount: 2230 },
-    { date: "2018-11-03", orderCount: 33, orderAmount: 3623 },
-    { date: "2018-11-04", orderCount: 50, orderAmount: 6423 },
-    { date: "2018-11-05", orderCount: 80, orderAmount: 8492 },
-    { date: "2018-11-06", orderCount: 60, orderAmount: 6293 },
-    { date: "2018-11-07", orderCount: 20, orderAmount: 2293 },
-    { date: "2018-11-08", orderCount: 60, orderAmount: 6293 },
-    { date: "2018-11-09", orderCount: 50, orderAmount: 5293 },
-    { date: "2018-11-10", orderCount: 30, orderAmount: 3293 },
-    { date: "2018-11-11", orderCount: 20, orderAmount: 2293 },
-    { date: "2018-11-12", orderCount: 80, orderAmount: 8293 },
-    { date: "2018-11-13", orderCount: 100, orderAmount: 10293 },
-    { date: "2018-11-14", orderCount: 10, orderAmount: 1293 },
-    { date: "2018-11-15", orderCount: 40, orderAmount: 4293 },
-  ],
-};
+// //画图用
+// const DATA_FROM_BACKEND = {
+//   columns: ["date", "orderCount", "orderAmount"],
+// };
 
 export default {
   data() {
@@ -245,8 +228,8 @@ export default {
             onClick(picker) {
               const end = new Date();
               let start = new Date();
-              start.setFullYear(2018);
-              start.setMonth(10);
+              start.setFullYear(2021);
+              start.setMonth(1);
               start.setDate(1);
               end.setTime(start.getTime() + 3600 * 1000 * 24 * 7);
               picker.$emit("pick", [start, end]);
@@ -257,8 +240,8 @@ export default {
             onClick(picker) {
               const end = new Date();
               let start = new Date();
-              start.setFullYear(2018);
-              start.setMonth(10);
+              start.setFullYear(2021);
+              start.setMonth(1);
               start.setDate(1);
               end.setTime(start.getTime() + 3600 * 1000 * 24 * 30);
               picker.$emit("pick", [start, end]);
@@ -301,7 +284,7 @@ export default {
           },
         }
       );
-      console.log(res);
+      //   console.log(res);
       if (res.data.code == 200) {
         this.todayOrderNum = res.data.data;
       }
@@ -317,7 +300,7 @@ export default {
           },
         }
       );
-      console.log(res);
+      //   console.log(res);
       if (res.data.code == 200) {
         this.todayOrderMoney = res.data.data;
       }
@@ -333,7 +316,7 @@ export default {
           },
         }
       );
-      console.log(res);
+      //   console.log(res);
       if (res.data.code == 200) {
         this.allOrderMoney = res.data.data;
         setTimeout(() => {
@@ -352,7 +335,7 @@ export default {
           },
         }
       );
-      console.log(res);
+      //   console.log(res);
       if (res.data.code == 200) {
         this.todayUserNum = res.data.data;
       }
@@ -364,35 +347,72 @@ export default {
     },
     initOrderCountDate() {
       let start = new Date();
-      start.setFullYear(2018);
-      start.setMonth(10);
+      start.setFullYear(2020);
+      start.setMonth(12);
       start.setDate(1);
       const end = new Date();
       end.setTime(start.getTime() + 1000 * 60 * 60 * 24 * 7);
       this.orderCountDate = [start, end];
     },
 
-    getData() {
-      setTimeout(() => {
-        this.chartData = {
-          columns: ["date", "orderCount", "orderAmount"],
-          rows: [],
-        };
-        for (let i = 0; i < DATA_FROM_BACKEND.rows.length; i++) {
-          let item = DATA_FROM_BACKEND.rows[i];
-          let currDate = str2Date(item.date);
-          let start = this.orderCountDate[0];
-          let end = this.orderCountDate[1];
-          if (
-            currDate.getTime() >= start.getTime() &&
-            currDate.getTime() <= end.getTime()
-          ) {
+    async getData() {
+      console.log("hello");
+      let start = new Date();
+      let end = new Date();
+      start.setTime(this.orderCountDate[0].getTime());
+      end.setTime(this.orderCountDate[1].getTime());
+      console.log(start);
+      console.log(end);
+      const PeriodOrderMoney = await axios.post(
+        `${api.API_URL}/statistics/PeriodOrderMoney`,
+        {
+          dateStart: start,
+          dateEnd: end,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        }
+      );
+      const periodOrder = await axios.post(
+        `${api.API_URL}/statistics/periodOrder`,
+        {
+          dateStart: start,
+          dateEnd: end,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        }
+      );
+      if (PeriodOrderMoney.data.code != 200 || periodOrder.data.code != 200) {
+        console.log("herehereerror");
+      } else {
+        setTimeout(() => {
+          this.chartData = {
+            columns: ["date", "orderCount", "orderAmount"],
+            rows: [],
+          };
+          console.log("PeriodOrderMoney", PeriodOrderMoney.data);
+          console.log("periodOrder", periodOrder.data);
+          for (var i in PeriodOrderMoney.data.data) {
+            //   console.log(i);
+            //   console.log(DATA_FROM_BACKEND.rows[i]);
+            var item = {
+              date: i,
+              orderAmount: PeriodOrderMoney.data.data[i],
+              orderCount: periodOrder.data.data[i],
+            };
+            console.log("item", item);
             this.chartData.rows.push(item);
           }
-        }
-        this.dataEmpty = false;
-        this.loading = false;
-      }, 1000);
+          this.dataEmpty = false;
+          this.loading = false;
+          console.log("chartData", this.chartData);
+        }, 1000);
+      }
     },
   },
 };
